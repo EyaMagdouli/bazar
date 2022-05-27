711591aff70e6fec0db4b03802b188b18a4cbebc
Eya Magdouli <eyamagdouli7@gmail.com>
2022-05-27 22:10:53 +0100

    backpack2

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -14,7 +14,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(
+            \Backpack\PermissionManager\app\Http\Controllers\UserCrudController::class, //this is package controller
+            \App\Http\Controllers\Admin\UserCrudController::class //this should be your own controller
+        );
     }
 
     /**
@@ -26,4 +29,5 @@ class AppServiceProvider extends ServiceProvider
     {
         Schema::defaultStringLength(191);
     }
+
 }
